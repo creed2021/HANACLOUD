1ed84142f86b27dbc6b448363a7b8ac10cd4d6ae
dante tagliavini <dantetagliavini@gmail.com>
2025-05-20 20:21:54 +0000

    alta

--- app/project2/webapp/controller/Alta.controller.js
+++ app/project2/webapp/controller/Alta.controller.js
@@ -7,7 +7,25 @@ sap.ui.define([
         onInit() {
         },
         onGuardar:function(){
-            
+      const oModel = this.getView().getModel(); // ODataModel
+      const oView = this.getView();
+
+      const nuevoEmpleado = {
+        NOMBRE: oView.byId("inputNombre").getValue(),
+        APELLIDO: oView.byId("inputApellido").getValue(),
+        SUELDO: parseFloat(oView.byId("inputSueldo").getValue())
+      };
+
+      oModel.create("/EmpleadosDirecto", nuevoEmpleado, {
+        success: () => {
+          sap.m.MessageToast.show("Empleado creado exitosamente");
+          //this.getOwnerComponent().getRouter().navTo("main");
+        },
+        error: (oError) => {
+          sap.m.MessageToast.show("Error al crear el empleado");
+          console.error(oError);
+        }
+      });
         }
     });
 });
